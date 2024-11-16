d1a0b2cb512ebe7cf0ceca15cb541d74febeef36
Lar <lara@ucr-secure-44-10-13-224-71.wnet.ucr.edu>
2024-11-16 13:29:20 -0800

    CSS

--- src/__init__.py
+++ src/__init__.py
@@ -4,9 +4,14 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.static_folder = 'static'
+
     # Import and register the Blueprint
     from .anna import anna
     app.register_blueprint(anna, url_prefix='/')
+
+    from .lara import lara
+    app.register_blueprint(lara, url_prefix='/')
 
     return app
 
