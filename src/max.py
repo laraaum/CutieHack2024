9c54308359353204a303444dc2ba0074057ace56
MacaroniMacks <122694751+MacaroniMacks@users.noreply.github.com>
2024-11-16 12:42:28 -0800

    Dab

--- src/max.py
+++ src/max.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import os
 
+max = Flask(__name__)
 
-
-
+@max.route('/', methods=['GET'])
+def logInPage():
+    return render_template('logInPage.html')
